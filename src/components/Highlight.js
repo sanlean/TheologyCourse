3633dcfe1c49313e113063e001687b5ed3fe2d88
Leo Santana <leo93santana@gmail.com>
2024-08-05 19:41:10 -0300

    🎉 first two subjects of course and Verse component in JS

---
+++ src/components/Highlight.js
@@ -0,0 +1,17 @@
+const Highlight = ({children, color}) => (
+    <span
+      style={{
+        backgroundColor: color,
+        borderRadius: '20px',
+        color: '#fff',
+        padding: '10px',
+        cursor: 'pointer',
+      }}
+      onClick={() => {
+        alert(`You clicked the color ${color} with label ${children}`);
+      }}>
+      {children}
+    </span>
+  );
+
+  export default Highlight;
